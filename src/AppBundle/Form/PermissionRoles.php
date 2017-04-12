df21dcfa289db0c495bdbdb91c49a8d56df36a75
Jake Israel <jcmisrael@gmail.com>
2017-04-12 17:35:47 -0400

    Adds forms for permission and permission matrix

---
+++ src/AppBundle/Form/PermissionRoles.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Permission;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PermissionRoles extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+	    $builder
+		    ->add('rolesFromArray', EntityType::class, array(
+		    	'class' => 'AppBundle\Entity\PermissionRole',
+			    'choice_label' => 'name',
+			    'expanded' => true,
+			    'multiple' => true,
+		    ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+	    $resolver->setDefaults(array(
+	    	'data_class' => Permission::class
+	    ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_role_permission_matrix';
+    }
+}
