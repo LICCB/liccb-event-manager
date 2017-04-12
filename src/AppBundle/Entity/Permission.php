199075d0f8b9abe3ef7fcb0a9ad55abb902f1e30
Jake Israel <jcmisrael@gmail.com>
2017-04-12 15:47:58 -0400

    Adds permission and permissionRole entities

---
+++ src/AppBundle/Entity/Permission.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jake
+ * Date: 4/12/17
+ * Time: 2:35 PM
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\Group as Base; // Extend group because it does what we want
+
+/**
+ * @author Jake Israel <jcmisrael@gmail.com>
+ */
+class Permission extends Base {
+
+	protected $id;
+
+	/**
+	 * Returns true or false whether a set of roles contains a valid permission role
+	 *
+	 * @param array $roles
+	 * @return bool
+	 */
+	public function hasRoleInArray($roles){
+		foreach ($roles as $role){
+			if($this->hasRole($role)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
