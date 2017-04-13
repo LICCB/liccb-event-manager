19e072d212c4a783448c0c823f94beb8b2ff19df
Larsen Fisch <larsen.fisch@gmail.com>
2017-04-13 13:22:19 -0400

    added min/max values for attendance form

--- src/AppBundle/Form/PartyAttendanceEdit.php
+++ src/AppBundle/Form/PartyAttendanceEdit.php
@@ -6,7 +6,7 @@ use AppBundle\Entity\Party;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,8 +16,10 @@ class PartyAttendanceEdit extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 	    $builder
-			->add('numActuallyAttended', NumberType::class, array(
-				'label' => false));
+			->add('numActuallyAttended', IntegerType::class, array(
+				'label' => false,
+                /* 'attr' => array('min' => 0)  min/max moved to twig page */
+                ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
