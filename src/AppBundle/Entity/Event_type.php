9537dc41a84cb9d7a863df164814f062cf6b32ac
Larsen Fisch <larsen.fisch@gmail.com>
2017-04-26 17:13:24 -0400

    added question entity and mappings with the event type

--- src/AppBundle/Entity/Event_type.php
+++ src/AppBundle/Entity/Event_type.php
@@ -51,5 +51,50 @@ class Event_type
     {
         return $this->eventType;
     }
-}
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $questions;
 
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->questions = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add question
+     *
+     * @param \AppBundle\Entity\Question $question
+     *
+     * @return Event_type
+     */
+    public function addQuestion(\AppBundle\Entity\Question $question)
+    {
+        $this->questions[] = $question;
+
+        return $this;
+    }
+
+    /**
+     * Remove question
+     *
+     * @param \AppBundle\Entity\Question $question
+     */
+    public function removeQuestion(\AppBundle\Entity\Question $question)
+    {
+        $this->questions->removeElement($question);
+    }
+
+    /**
+     * Get questions
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getQuestions()
+    {
+        return $this->questions;
+    }
+}
